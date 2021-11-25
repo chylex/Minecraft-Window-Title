04f1714ec5afb974cf2060980e4c81160be014ea
chylex <contact@chylex.com>
2021-11-25 16:08:33 +0100

    Fix incompatible server icon in Forge's multiplayer menu
    Closes #4

--- Forge/src/main/java/chylex/customwindowtitle/forge/CustomWindowTitle.java
+++ Forge/src/main/java/chylex/customwindowtitle/forge/CustomWindowTitle.java
@@ -4,10 +4,13 @@ import chylex.customwindowtitle.TitleParser;
 import com.mojang.blaze3d.platform.Window;
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.IExtensionPoint.DisplayTest;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLPaths;
+import net.minecraftforge.fmllegacy.network.FMLNetworkConstants;
 
 @Mod("customwindowtitle")
 public class CustomWindowTitle {
@@ -15,6 +18,7 @@ public class CustomWindowTitle {
 	
 	public CustomWindowTitle() {
 		config = TitleConfig.read(FMLPaths.CONFIGDIR.get().toString());
+		ModLoadingContext.get().registerExtensionPoint(DisplayTest.class, () -> new DisplayTest(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
 		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::onClientSetup);
 		TokenData.register();
 	}
