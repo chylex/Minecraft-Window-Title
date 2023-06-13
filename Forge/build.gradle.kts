8190b1e1c13a55155efdccd4b24b9ea5b5667ba0
chylex <contact@chylex.com>
2023-06-13 15:17:49 +0200

    Revert to Gradle 7 and ForgeGradle 5

--- Forge/build.gradle.kts
+++ Forge/build.gradle.kts
@@ -15,7 +15,7 @@ buildscript {
 	}
 	
 	dependencies {
-		classpath(group = "net.minecraftforge.gradle", name = "ForgeGradle", version = "[6.0,6.2)")
+		classpath(group = "net.minecraftforge.gradle", name = "ForgeGradle", version = "5.1.+") { isChanging = true }
 		classpath(group = "org.spongepowered", name = "mixingradle", version = "0.7-SNAPSHOT")
 	}
 }
@@ -40,8 +40,6 @@ dependencies {
 
 configure<UserDevExtension> {
 	mappings("official", minecraftVersion)
-	
-	copyIdeResources.set(true)
 	
 	runs {
 		create("client") {
