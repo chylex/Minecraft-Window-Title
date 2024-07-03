ef0031a3dd35ac9f68489ecabad89aa4b1109de0
chylex <contact@chylex.com>
2024-07-03 05:45:15 +0200

    Update for Minecraft 1.21

--- NeoForge/src/main/java/chylex/customwindowtitle/neoforge/CustomWindowTitle.java
+++ NeoForge/src/main/java/chylex/customwindowtitle/neoforge/CustomWindowTitle.java
@@ -4,22 +4,20 @@ import chylex.customwindowtitle.TitleConfig;
 import chylex.customwindowtitle.TitleParser;
 import chylex.customwindowtitle.data.CommonTokenData;
 import net.minecraft.client.Minecraft;
+import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.IExtensionPoint.DisplayTest;
-import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
-import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.neoforged.fml.loading.FMLPaths;
 
 @Mod("customwindowtitle")
 public class CustomWindowTitle {
+	
 	private final TitleConfig config;
 	
-	public CustomWindowTitle() {
+	public CustomWindowTitle(IEventBus eventBus) {
 		config = TitleConfig.read(FMLPaths.CONFIGDIR.get().toString());
-		ModLoadingContext.get().registerExtensionPoint(DisplayTest.class, () -> new DisplayTest(() -> DisplayTest.IGNORESERVERONLY, (a, b) -> true));
-		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::onClientSetup);
+		eventBus.addListener(this::onClientSetup);
 		CommonTokenData.register(new TokenProvider());
 	}
 	
