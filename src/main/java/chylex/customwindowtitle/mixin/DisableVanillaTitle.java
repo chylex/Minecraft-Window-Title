c62d8658c4545e4b82b805c767cfc944d75b6a6b
Kolotheegg <105158781+Kolotheegg@users.noreply.github.com>
2024-07-28 21:23:37 +0200

    Reimplemented Icon Support. Used Lambda functions for config reading

--- src/main/java/chylex/customwindowtitle/mixin/DisableVanillaTitle.java
+++ src/main/java/chylex/customwindowtitle/mixin/DisableVanillaTitle.java
@@ -1,5 +1,7 @@
 package chylex.customwindowtitle.mixin;
 
+import chylex.customwindowtitle.IconChanger;
+import chylex.customwindowtitle.TitleConfig;
 import net.minecraft.client.Minecraft;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -8,8 +10,17 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Minecraft.class)
 public final class DisableVanillaTitle {
+
 	@Inject(method = "updateTitle()V", at = @At("HEAD"), cancellable = true)
 	private void updateTitle(final CallbackInfo info) {
 		info.cancel();
+	}
+
+	@Inject(method = "onResourceLoadFinished", at = @At("HEAD"))
+	private void onFinishedLoading(final CallbackInfo callbackInfo) {
+		final TitleConfig config = TitleConfig.getInstance();
+		if (config != null && config.hasIcon()) {
+			IconChanger.setIcon(config.getIconPath());
+		}
 	}
 }
